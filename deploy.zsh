677b7bbc9123d4965f404fefd09285fd05a6918d
EasyOnHard <xander.iwanowicz@gmail.com>
2025-06-06 14:49:50 -0400

    Collecting all Files

--- deploy.zsh
+++ deploy.zsh
@@ -4,6 +4,7 @@ rsync -av --delete ~/Documents/Notes/Wiki ~/Documents/Hugo-Obsidian-Site/blog/co
 
 cd ~/Documents/Hugo-Obsidian-Site/blog/
 git add .
+git commit -m "Collecting all Files"
 git subtree split --prefix=public -b deploy
 git checkout deploy
 git commit -m "Updating Pages with New Content"
